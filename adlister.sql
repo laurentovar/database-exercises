1d2a4e01f9205530c9ffd7dbd0bb39ddee4fdbae
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2020-01-18 15:11:11 -0600

    adlister sql

---
+++ adlister.sql
@@ -0,0 +1,20 @@
+USE adlister_db;
+
+--
+CREATE  TABLE categories
+(
+    id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE ads
+ (
+     id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     user_id INT UNSIGNED DEFAULT NULL,
+     category_id INT UNSIGNED DEFAULT NULL,
+     title VARCHAR(100) NOT NULL,
+     description VARCHAR(500) NOT NULL,
+     PRIMARY KEY (id),
+     FOREIGN KEY (user_id) REFERENCES users (id),
+     FOREIGN KEY (category_id) REFERENCES categories (id)
+ );
