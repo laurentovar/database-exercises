e5a5c30d6d16c9c85394e7bc1d80eccd3a63f591
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2020-01-22 10:11:18 -0600

    adlister updated

--- adlister.sql
+++ adlister.sql
@@ -1,22 +1,40 @@
-USE adlister_db;
 
---
+use adlister_db;
+DROP TABLE ads;
+DROP TABLE categories;
+DROP TABLE users;
+#
+CREATE TABLE users(
+                      id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                      username VARCHAR(100) NOT NULL,
+                      email VARCHAR(100) NOT NULL,
+                      password VARCHAR(100) NOT NULL,
+                      PRIMARY KEY (id)
+);
 CREATE  TABLE categories
 (
     id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
     category VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
-CREATE TABLE ads
- (
-     id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
-     user_id INT UNSIGNED DEFAULT NULL,
-     category_id INT UNSIGNED DEFAULT NULL,
-     title VARCHAR(100) NOT NULL,
-     description VARCHAR(500) NOT NULL,
-     PRIMARY KEY (id),
-     FOREIGN KEY (user_id) REFERENCES users (id),
-     FOREIGN KEY (category_id) REFERENCES categories (id)
- );
 
-DROP TABLE users;
+CREATE TABLE ads
+(
+    id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED DEFAULT NULL,
+    category_id INT UNSIGNED DEFAULT NULL,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (category_id) REFERENCES categories (id)
+);
+INSERT INTO categories (category) VALUES ('Music');
+INSERT INTO categories (category) VALUES ('Sports');
+INSERT INTO categories (category) VALUES ('Arts');
+INSERT INTO categories (category) VALUES ('Math');
+INSERT INTO categories (category) VALUES ('Science');
+INSERT INTO categories (category) VALUES ('History');
+INSERT INTO categories (category) VALUES ('Languages');
+INSERT INTO categories (category) VALUES ('Programming');
+INSERT INTO categories (category) VALUES ('Other');
